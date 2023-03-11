2ce7f3cf677e14f89235f9f9549ad934f280a009
devkult <devkultanov@gmail.com>
2023-03-11 15:34:32 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,46 @@
+import sys
+from PyQt5 import QtGui, QtCore
+from PyQt5.QtCore    import QUrl , Qt 
+from PyQt5.QtWidgets import QMainWindow, QApplication , QDesktopWidget
+from PyQt5.QtWebEngineWidgets import QWebEngineView
+import os
+
+current_dir = os.getcwd().replace("\\", "/")
+print(current_dir)
+
+
+
+class MainWindow(QMainWindow):
+    def __init__(self ):
+        super(QMainWindow, self).__init__()
+        self.setWindowTitle('Пример загрузки и отображения локальной страницы')
+
+        self.browser = QWebEngineView()  
+
+        # Загрузка локальной страницы
+        # Полный путь к index.html
+        url = current_dir + '/index.html'
+        self.browser.load(QUrl( url ))  
+        self.setCentralWidget(self.browser)
+        self.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint | QtCore.Qt.FramelessWindowHint)
+    
+
+    def location_on_the_screen(self):
+        ag = QDesktopWidget().availableGeometry()
+        sg = QDesktopWidget().screenGeometry()
+
+        widget = self.geometry()
+        x = ag.width() - widget.width()
+        y = 2 * ag.height() - sg.height() - widget.height()
+        self.move(x, y)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)       
+    win = MainWindow()
+    win.location_on_the_screen()
+    win.show()
+    sys.exit(app.exec_())
+
+print("Программа выполнилась успешно")
+input("Нажмите любую клавишу для выхода")
---
+++ script.js
@@ -0,0 +1,63 @@
+// var cnt=document.getElementById("count"); 
+// var water=document.getElementById("water");
+// var percent=cnt.innerText;
+// var interval;
+// interval=setInterval(function(){ 
+//   percent++; 
+//   cnt.innerHTML = percent; 
+//   water.style.transform='translate(0'+','+(100-percent)+'%)';
+//   if(percent==100){
+//     clearInterval(interval);
+//   }
+// },60);
+
+
+
+
+  // определяем переменную, которая будет хранить максимальную сумму
+  var maxSum = 150000;
+
+  // определяем функцию, которая будет считывать значение с JSON-файла и отображать его в виде заполненной жидкости
+  function updateLiquid() {
+    // отправляем запрос на получение данных с JSON-файла
+    fetch('http://206.81.30.246/data.json')
+      .then(response => response.json()) // преобразуем полученный ответ в объект JavaScript
+      .then(data => { 
+        
+        var currentSum = data.sum; // получаем текущую сумму из данных
+        console.log(data.donation)
+        // вычисляем процент заполнения
+        var percent = Math.floor(currentSum / maxSum * 100);
+  
+        // отображаем процент заполнения в элементе с идентификатором "count"
+        var cnt = document.getElementById("count");
+        cnt.innerText = currentSum + "/";
+        var top = document.getElementById("top-donation");
+        var str =''
+        var step = 0
+        for (let key in data.donation) {
+          if (step === 5) {
+            break
+          }
+          str += `<div class="donation"><div class="top">${key}:</div><div class="bottom">${data.donation[key]} ₸</div></div>`
+          step++
+          console.log(step)
+        }
+        top.innerHTML = str
+        // изменяем стиль элемента с идентификатором "water" для отображения заполнения жидкости
+        var water = document.getElementById("water");
+        water.style.transform = 'translate(0,' + (100 - percent) + '%)';
+        
+        console.log(currentSum);
+      })
+      .catch(error => console.error(error));
+  }
+  
+  // вызываем функцию в первый раз, чтобы отобразить начальное состояние
+  updateLiquid();
+  
+  // запускаем функцию updateLiquid каждые 60 миллисекунд
+  setInterval(updateLiquid, 10000  );
+  
+  
+  
